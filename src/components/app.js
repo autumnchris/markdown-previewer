aaa6c11e696b7a2671a1895e737bd939b5580327
Autumn <autumncbullard@gmail.com>
2017-10-12 23:25:35 -0400

    Add the React-Bootstrap props Tabs and Tab to toggle between the textarea and the preview

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,16 @@ export default class App extends Component {
         <header>
           <h1 className="text-center" id="app-heading">Markdown Previewer</h1>
         </header>
+        <main>
+          <Tabs defaultActiveKey={1} animation={false} id="tabs">
+            <Tab eventKey={1} title=" Edit">
+              <textarea className="form-control" rows="20" />
+            </Tab>
+            <Tab eventKey={2} title=" Preview">
+              <div></div>
+            </Tab>
+          </Tabs>
+        </main>
         <footer className="text-center">Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
       </div>
     );
