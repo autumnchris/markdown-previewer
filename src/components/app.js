4411961c7129d65e378ebb0311c3be3de91d113a
Autumn <autumncbullard@gmail.com>
2017-10-25 19:32:14 -0400

    Fix web accessibility errors

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdownInput: '# Heading\n## Sub-heading\n### Another deeper heading\nParagraphs are separated by a blank line.\n\nTwo spaces at the end of a line leave a line break.\n\nText attributes _italic_, *italic*, __bold__, **bold**, `monospace`, ~~strikethrough~~.\n\nHorizontal rule:\n\n---\n\nBullet list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears\n\nA [link](#).'
+      markdownInput: '# Heading\n## Sub-heading\n### Another deeper heading\nParagraphs are separated by a blank line.\n\nTwo spaces at the end of a line leave a line break.\n\nText attributes _italic_, *italic*, __bold__, **bold**, `monospace`, ~~strikethrough~~.\n\nHorizontal rule:\n\n---\n\nBullet list:\n* apples\n* oranges\n* pears\n\nNumbered list:\n1. apples\n2. oranges\n3. pears'
     };
   }
 
@@ -26,7 +26,7 @@ export default class App extends Component {
         <main>
           <Tabs defaultActiveKey={1} animation={false} id="tabs">
             <Tab eventKey={1} title=" Edit">
-              <textarea className="form-control" rows="20" onChange={this.handleChange.bind(this)} value={this.state.markdownInput} />
+              <textarea className="form-control" rows="20" aria-label="type GitHub Flavored Markdown text" onChange={this.handleChange.bind(this)} value={this.state.markdownInput} />
             </Tab>
             <Tab eventKey={2} title=" Preview">
               <div dangerouslySetInnerHTML={{__html: marked(this.state.markdownInput)}} id="preview"></div>
