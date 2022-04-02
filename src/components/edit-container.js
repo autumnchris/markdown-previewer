8ae61c1c3a976fd2059b0b8d1b32fa3b6be3dbc8
autumnchris <autumncbullard@gmail.com>
2022-04-02 17:06:02 -0400

    Add a button that copies Markdown code to the clipboard

--- src/components/edit-container.js
+++ src/components/edit-container.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const EditContainer = ({ markdownInput, handleChange }) => {
   return (
-    <form className="markdown-form">
-      <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" />
+    <React.Fragment>
+      <form className="markdown-form">
+      <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" />
     </form>
+    <div className="button-group">
+      <button type="button" className="button" onClick={() => navigator.clipboard.writeText(markdownInput)}>Copy to Clipboard</button>
+    </div>
+    </React.Fragment>
   );
 }
 
