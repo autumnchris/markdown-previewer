01f7af1a560a0656651674e2767ec30d6e2355b4
autumnchris <autumncbullard@gmail.com>
2023-01-17 12:11:45 -0500

    Prevent form from submitting

--- src/components/EditContainer.js
+++ src/components/EditContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const EditContainer = ({ markdownInput, handleChange }) => {
   return (
     <React.Fragment>
-      <form className="markdown-form">
+      <form className="markdown-form" onSubmit={(event) => { event.preventDefault(); }}>
       <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" />
     </form>
     <div className="button-group">
