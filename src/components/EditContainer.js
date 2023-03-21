ba10dd2934b1d0ba294a604fc259181309433d72
autumnchris <autumncbullard@gmail.com>
2023-03-20 23:23:38 -0400

    Fix indentation

--- src/components/EditContainer.js
+++ src/components/EditContainer.js
@@ -4,11 +4,11 @@ const EditContainer = ({ markdownInput, handleChange }) => {
   return (
     <React.Fragment>
       <form className="markdown-form" onSubmit={(event) => { event.preventDefault(); }}>
-      <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" />
-    </form>
-    <div className="button-group">
-      <button type="button" className="button" onClick={() => navigator.clipboard.writeText(markdownInput)}>Copy to Clipboard</button>
-    </div>
+        <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" />
+      </form>
+      <div className="button-group">
+        <button type="button" className="button" onClick={() => navigator.clipboard.writeText(markdownInput)}>Copy to Clipboard</button>
+      </div>
     </React.Fragment>
   );
 }
