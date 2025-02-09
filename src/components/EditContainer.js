5675e65a74090388a91f7bde32ec62be4c4c9df3
autumnchris <autumncbullard@gmail.com>
2025-02-08 23:07:41 -0500

    Add autocomplete attribute to textarea

--- src/components/EditContainer.js
+++ src/components/EditContainer.js
@@ -9,7 +9,7 @@ const EditContainer = ({ markdownInput, handleChange }) => {
   return (
     <React.Fragment>
       <form className="markdown-form" onSubmit={(event) => { event.preventDefault(); }}>
-        <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" />
+        <textarea className="markdown-input" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" autoComplete="off" />
       </form>
       <div className="button-group">
         <button type="button" className="button" onClick={() => copyText(markdownInput)}><span className="far fa-copy fa-fw" aria-hidden="true"></span> Copy to Clipboard</button>
