810a5b5762c32ad9bbe269964ddcf982bb14fed6
autumnchris <autumncbullard@gmail.com>
2025-02-17 17:38:33 -0500

    Improve aria labels

--- src/components/EditContainer.js
+++ src/components/EditContainer.js
@@ -12,7 +12,7 @@ const EditContainer = ({ markdownInput, handleChange }) => {
         <textarea className="markdown-input" name="markdownCode" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" autoComplete="off" />
       </form>
       <div className="button-group">
-        <button type="button" className="button" onClick={() => copyText(markdownInput)}><span className="fa-regular fa-copy fa-fw" aria-hidden="true"></span> Copy to Clipboard</button>
+        <button type="button" className="button" onClick={() => copyText(markdownInput)} aria-label="copy code to clipboard"><span className="fa-regular fa-copy fa-fw" aria-hidden="true"></span> Copy to Clipboard</button>
       </div>
     </React.Fragment>
   );
