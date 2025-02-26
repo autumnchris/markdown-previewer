bb306d0036a3365d925926942e38d1a2ce0d07e8
autumnchris <autumncbullard@gmail.com>
2025-02-26 12:26:11 -0500

    Add autocapitalize=off to textarea

--- src/components/EditContainer.js
+++ src/components/EditContainer.js
@@ -9,7 +9,7 @@ const EditContainer = ({ markdownInput, handleChange }) => {
   return (
     <React.Fragment>
       <form className="markdown-form" onSubmit={(event) => { event.preventDefault(); }} noValidate>
-        <textarea className="markdown-input" name="markdownCode" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" autoComplete="off" />
+        <textarea className="markdown-input" name="markdownCode" onChange={(event) => handleChange(event)} value={markdownInput} rows="20" placeholder="Enter GitHub Flavored Markdown syntax to preview..." aria-label="Enter GitHub Flavored Markdown syntax to preview" id="markdown-code" autoComplete="off" autoCapitalize="off" />
       </form>
       <div className="button-group">
         <button type="button" className="button" onClick={() => copyText(markdownInput)} aria-label="copy code to clipboard"><span className="fa-regular fa-copy fa-fw" aria-hidden="true"></span> Copy to Clipboard</button>
